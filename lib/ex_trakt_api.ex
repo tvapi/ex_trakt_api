1d13f18677db5797a42cebdbbe48fce7fa54e268
Krzysztof Wawer <krzysztof.wawer@gmail.com>
2019-02-02 23:16:45 +0100

    Initial commit

---
+++ lib/ex_trakt_api.ex
@@ -0,0 +1,18 @@
+defmodule ExTraktApi do
+  @moduledoc """
+  Documentation for ExTraktApi.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExTraktApi.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ex_trakt_api_test.exs
@@ -0,0 +1,8 @@
+defmodule ExTraktApiTest do
+  use ExUnit.Case
+  doctest ExTraktApi
+
+  test "greets the world" do
+    assert ExTraktApi.hello() == :world
+  end
+end
